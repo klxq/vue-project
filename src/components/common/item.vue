82164bf098383aba79982f438fb7db1f87caca4e
wl.klxq@gmail.com <wl.klxq@gmail.com>
2017-02-17 00:59:41 +0800

    [Ling]Add style for list

--- src/components/common/item.vue
+++ src/components/common/item.vue
@@ -1,10 +1,19 @@
 <template>
-  <router-link :to="'/film/' + item.id">
-    <div class="film">
-      <img v-if="item.images" :src="imgProxy(item.images.small)">
-      <p>{{ item.title }}</p>
+  <div class="film-item">
+    <div class="film-item--image">
+      <router-link :to="'/film/' + item.id">
+        <img v-if="item.images" :src="item.images.large">
+      </router-link>
     </div>
-  </router-link>
+    <div class="film-item--footer">
+      <span class="film-item--title">
+        <router-link :to="'/film/' + item.id">
+          {{ item.title }}
+        </router-link>
+      </span>
+      <span class="film-item--rate">{{ item.rating.average }}</span>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
@@ -21,23 +30,54 @@
 </script>
 
 <style lang="less" rel="stylesheet/less">
-  .film {
-    margin: 20px;
-    box-shadow: 0 0 4px rgba(0, 0, 0, 0.25);
-    cursor: pointer;
-    position: relative;
+  .film-item {
+    display: inline-block;
+    float: left;
+    width: 220px;
+    margin: 5px 15px 15px 0;
 
-    p {
-      display: inline-block;
-      width: 400px;
-      padding-left: 20px;
-      vertical-align: bottom;
-      font-size: 1.2rem;
-      }
-    em {
-      position: absolute;
-      right: 10px;
-      bottom: 10px;
+
+    .film-item--image {
+      img {
+        display: block;
+        width: 220px;
+        height: 300px;
+        transition: all .5s ease-in-out;
+
+        &:hover {
+          transform: scale(1.05);
+          transition: all .5s ease-in-out;
+        }
       }
     }
+    .film-item--footer {
+      position: relative;
+
+      .film-item--title {
+        display: block;
+        height: 24px;
+        line-height: 24px;
+        margin-top: 15px;
+        text-align: left;
+
+        & > a {
+          text-decoration: none;
+          font-weight: bold;
+          font-size: 16px;
+          color: #222;
+
+          &:hover {
+            color: #e1b355;
+          }
+        }
+      }
+      .film-item--rate {
+        position: absolute;
+        font-weight: bold;
+        right: 0;
+        bottom: 0;
+        color: #fa5058;
+      }
+    }
+  }
 </style>
