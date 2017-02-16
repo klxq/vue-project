4ac1f858ff4cdb26011546a06f220a02b52a38b5
soonberry <shuwenli89@126.com>
2017-02-16 23:26:57 +0800

    [Shuwen] Add style for film content.

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 60px;
+  background: #e5ebe4;
+}
+* {
+  margin: 0;
 }
 </style>
